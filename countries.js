d33767139ae31354908ed78d081c33ce01c5543f
Samin Yasar <79013179+Samin-yasar@users.noreply.github.com>
2025-06-21 22:21:30 +0600

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,3 +1,21 @@
+/*
+ * OriginScan
+ * Copyright (C) 2025 Samin Yasar <[YOUR EMAIL ADDRESS, optional]>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const countryData = {
   // United States (USA) prefixes
   "000-019": { name: "United States (UPC-A compatible)", flag: "🇺🇸", apiLookupName: "United States", region: "North America", currency: "United States Dollar (USD)" },
